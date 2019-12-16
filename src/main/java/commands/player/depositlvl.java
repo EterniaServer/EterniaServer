d63d9580e3d0bd72b805fc6d0eb7f1c0c4a068eb
yurinogueira <yurinogueira@id.uff.br>
2019-12-16 05:13:06 -0300

    Corrigido SQL.

--- src/main/java/commands/player/depositlvl.java
+++ src/main/java/commands/player/depositlvl.java
@@ -1,16 +1,21 @@
 package commands.player;
-import center.sql.xpmanager;
+import center.main;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.UUID;
 
 public class depositlvl implements CommandExecutor
 {
-    final private xpmanager xpm = new xpmanager();
+    private final main plugin = main.getPlugin(main.class);
     @EventHandler
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
@@ -27,10 +32,46 @@ public class depositlvl implements CommandExecutor
                     if (xp_atual >= Integer.parseInt(args[0]))
                     {
                         xp_atual = Integer.parseInt(args[0]);
-                        xpm.deposit_xp(uuid, player);
                         player.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "E" + ChatColor.BLUE +
                                 "S" + ChatColor.DARK_GRAY + "] " + ChatColor.GRAY + "Pronto, você tem " + ChatColor.DARK_AQUA
                                 + xp_atual + ChatColor.GRAY + " leveis guardados" + ChatColor.DARK_GRAY + ".");
+                        try
+                        {
+                            int xp = xp_atual;
+                            if (xp >= 1 && xp <= 16)
+                            {
+                                xp = xp * (6 + xp);
+                            }
+                            else if (xp >= 17 && xp <= 25)
+                            {
+                                xp = xp * (8 + xp);
+                            }
+                            else if (xp >= 26 && xp <= 30)
+                            {
+                                xp = xp * (16 + xp);
+                            }
+                            else
+                            {
+                                xp = xp * (33 + xp);
+                            }
+                            Statement statement = plugin.getConnection().createStatement();
+                            ResultSet results = statement.executeQuery("SELECT XP FROM " + plugin.table + " WHERE UUID = '"+uuid.toString()+"'");
+                            String Vu = "";
+                            while (results.next())
+                            {
+                                Vu = results.getString("XP");
+                            }
+                            int XP = Integer.parseInt(Vu);
+                            xp = Math.max(xp + XP, 0);
+                            results.close();
+                            statement.executeUpdate("UPDATE " + plugin.table + " SET XP='"+xp+"' WHERE UUID='"+uuid.toString()+"'");
+                            player.setLevel(Math.max(player.getLevel() - xp_atual, 0));
+                            statement.close();
+                        }
+                        catch(SQLException e)
+                        {
+                            e.printStackTrace();
+                        }
                         return true;
                     } else {
                         player.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "E" + ChatColor.BLUE +
--- src/main/java/commands/player/withdrawlvl.java
+++ src/main/java/commands/player/withdrawlvl.java
@@ -1,16 +1,21 @@
 package commands.player;
-import center.sql.xpmanager;
+import center.main;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.UUID;
 
 public class withdrawlvl implements CommandExecutor
 {
-    final private xpmanager xpm = new xpmanager();
+    private final main plugin = main.getPlugin(main.class);
     @EventHandler
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
@@ -22,10 +27,46 @@ public class withdrawlvl implements CommandExecutor
             try
             {
                 int xp1 = player.getLevel();
-                xpm.withdraw_xp(uuid, player);
                 player.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "E" + ChatColor.BLUE +
                         "S" + ChatColor.DARK_GRAY + "] " + ChatColor.GRAY + "Pronto seu level agora é " + ChatColor.DARK_AQUA
                         + xp1 + ChatColor.DARK_GRAY + ".");
+                try
+                {
+                    Statement statement = plugin.getConnection().createStatement();
+                    ResultSet results = statement.executeQuery("SELECT XP FROM " + plugin.table + " WHERE UUID = '"+uuid.toString()+"'");
+                    String Vu = "";
+                    while (results.next())
+                    {
+                        Vu = results.getString("XP");
+                    }
+                    results.close();
+                    int XP = Integer.parseInt(Vu);
+                    if (XP >= 1 && XP <= 352)
+                    {
+                        XP = XP / 10;
+                    }
+                    else if (XP >= 353 && XP <= 910)
+                    {
+                        XP = XP / (18);
+                    }
+                    else if (XP >= 911 && XP <= 1395)
+                    {
+                        XP = XP / (26);
+                    }
+                    else
+                    {
+                        XP = XP / (60);
+                    }
+                    XP = Math.max(XP + player.getLevel(), 0);
+                    player.setLevel(XP);
+                    int v = 0;
+                    statement.executeUpdate("UPDATE " + plugin.table + " SET XP='"+v+"' WHERE UUID='"+uuid.toString()+"'");
+                    statement.close();
+                }
+                catch(SQLException e)
+                {
+                    e.printStackTrace();
+                }
                 return true;
             }
             catch (Exception e)
