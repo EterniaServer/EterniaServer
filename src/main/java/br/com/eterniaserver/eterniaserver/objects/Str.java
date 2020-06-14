3fccb5cf74a75633c24641cd0c897f1053ee224f
yurinogueira <yurinogueira@id.uff.br>
2020-06-14 18:49:57 -0300

    Eu era burro, não sabia da existencia de interfaces.

--- src/main/java/br/com/eterniaserver/eterniaserver/objects/Str.java
+++ src/main/java/br/com/eterniaserver/eterniaserver/objects/Str.java
@@ -1,20 +1,20 @@
 package br.com.eterniaserver.eterniaserver.objects;
 
-public class Str {
+public interface Str {
 
     // Placeholders
-    public static final String strPlayNamP = "%player_name%";
-    public static final String stringTimeP = "%time%";
+    String strPlayNamP = "%player_name%";
+    String stringTimeP = "%time%";
 
     // Server
-    public static final String mChatStr = "modules.chat";
-    public static final String svChecks = "server.checks";
-    public static final String svCooldo = "server.cooldown";
+    String mChatStr = "modules.chat";
+    String svChecks = "server.checks";
+    String svCooldo = "server.cooldown";
 
     // SQL
-    public static final String tbCreate = "CREATE TABLE IF NOT EXISTS ";
-    public static final String qInsertI = "INSERT INTO ";
-    public static final String qSeleAll = "SELECT * FROM ";
-    public static final String qWhePlNa = " WHERE player_name='";
+    String tbCreate = "CREATE TABLE IF NOT EXISTS ";
+    String qInsertI = "INSERT INTO ";
+    String qSeleAll = "SELECT * FROM ";
+    String qWhePlNa = " WHERE player_name='";
 
 }
