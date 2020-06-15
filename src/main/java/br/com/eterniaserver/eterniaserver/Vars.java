4a6773b6a3b04a8f79b038ea89d8c554b1581292
yurinogueira <yurinogueira@id.uff.br>
2020-06-14 22:13:36 -0300

    Mais um teste.

--- src/main/java/br/com/eterniaserver/eterniaserver/Vars.java
+++ src/main/java/br/com/eterniaserver/eterniaserver/Vars.java
@@ -13,24 +13,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Vars extends JavaPlugin {
 
     // Placeholders
-    public final String strPlayNamP = "%player_name%";
-    public final String stringTimeP = "%time%";
+    public static final String strPlayNamP = "%player_name%";
+    public static final String stringTimeP = "%time%";
     // Server
-    public final String mChatStr = "modules.chat";
-    public final String svChecks = "server.checks";
-    public final String svCooldo = "server.cooldown";
+    public static final String mChatStr = "modules.chat";
+    public static final String svChecks = "server.checks";
+    public static final String svCooldo = "server.cooldown";
     // Tables
-    public final String tbHomeSt = "sql.table-home";
+    public static final String tbHomeSt = "sql.table-home";
     // SQL
-    public final String tbCreate = "CREATE TABLE IF NOT EXISTS ";
-    public final String qInsertI = "INSERT INTO ";
-    public final String qSeleAll = "SELECT * FROM ";
-    public final String qUpdateS = "UPDATE ";
-    public final String qWhePlNa = " WHERE player_name='";
+    public static final String tbCreate = "CREATE TABLE IF NOT EXISTS ";
+    public static final String qInsertI = "INSERT INTO ";
+    public static final String qSeleAll = "SELECT * FROM ";
+    public static final String qUpdateS = "UPDATE ";
+    public static final String qWhePlNa = " WHERE player_name='";
 
     public final Location error = new Location(Bukkit.getWorld("world"), 666, 666, 666, 666, 666);
 
@@ -44,25 +45,25 @@ class Vars extends JavaPlugin {
     public final List<String> playerHomes = new ArrayList<>();
     public final List<String> playerCooldown = new ArrayList<>();
 
-    public final HashMap<String, Long> tpaTime = new HashMap<>();
-    public final HashMap<String, Long> playerMuted = new HashMap<>();
-    public final HashMap<String, Long> bedCooldown = new HashMap<>();
-    public final HashMap<String, Long> afktime = new HashMap<>();
-    public final HashMap<String, Double> balances = new HashMap<>();
-    public final HashMap<String, String> tpaRequests = new HashMap<>();
-    public final HashMap<String, String> kitsCooldown = new HashMap<>();
-    public final HashMap<String, String> playerLogin = new HashMap<>();
-    public final HashMap<String, String> tell = new HashMap<>();
-    public final HashMap<Player, Boolean> spy = new HashMap<>();
-    public final HashMap<String, Integer> global = new HashMap<>();
-    public final HashMap<String, Integer> xp = new HashMap<>();
-    public final HashMap<String, Integer> playersInPortal = new HashMap<>();
-    public final HashMap<String, String[]> home = new HashMap<>();
-    public final HashMap<String, Location> warps = new HashMap<>();
-    public final HashMap<String, Location> back = new HashMap<>();
-    public final HashMap<String, Location> shops = new HashMap<>();
-    public final HashMap<String, Location> homes = new HashMap<>();
-    public final HashMap<String, FormatInfo> uufi = new HashMap<>();
-    public final HashMap<Player, PlayerTeleport> teleports = new HashMap<>();
+    public final Map<String, Long> tpaTime = new HashMap<>();
+    public final Map<String, Long> playerMuted = new HashMap<>();
+    public final Map<String, Long> bedCooldown = new HashMap<>();
+    public final Map<String, Long> afktime = new HashMap<>();
+    public final Map<String, Double> balances = new HashMap<>();
+    public final Map<String, String> tpaRequests = new HashMap<>();
+    public final Map<String, String> kitsCooldown = new HashMap<>();
+    public final Map<String, String> playerLogin = new HashMap<>();
+    public final Map<String, String> tell = new HashMap<>();
+    public final Map<Player, Boolean> spy = new HashMap<>();
+    public final Map<String, Integer> global = new HashMap<>();
+    public final Map<String, Integer> xp = new HashMap<>();
+    public final Map<String, Integer> playersInPortal = new HashMap<>();
+    public final Map<String, String[]> home = new HashMap<>();
+    public final Map<String, Location> warps = new HashMap<>();
+    public final Map<String, Location> back = new HashMap<>();
+    public final Map<String, Location> shops = new HashMap<>();
+    public final Map<String, Location> homes = new HashMap<>();
+    public final Map<String, FormatInfo> uufi = new HashMap<>();
+    public final Map<Player, PlayerTeleport> teleports = new HashMap<>();
 
 }
