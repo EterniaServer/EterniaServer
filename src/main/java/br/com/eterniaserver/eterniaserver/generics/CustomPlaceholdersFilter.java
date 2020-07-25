c86e33d763252c0db33325bcbc7eaeffa6a4eefb
yurinogueira <yurinogueira@id.uff.br>
2020-07-25 02:25:40 -0300

    CustomPlaceholdersFilter

--- src/main/java/br/com/eterniaserver/eterniaserver/generics/CustomPlaceholdersFilter.java
+++ src/main/java/br/com/eterniaserver/eterniaserver/generics/CustomPlaceholdersFilter.java
@@ -25,25 +25,29 @@ public class CustomPlaceholdersFilter extends StringHelper {
 					if (cp.isNotIndependent() && bestPlaceholder != null) {
 						chatObj.setMessage(objMsg.replace("{" + cp.getId() + "}", cc(plugin.getInternMethods().setPlaceholders(p, bestPlaceholder.getValue()))));
 					} else {
-						int i2 = objMsg.indexOf("{" + cp.getId() + "}");
-						int i3 = i2 + ("{" + cp.getId() + "}").length();
-						chatObj.setMessage(objMsg.substring(0, i2));
-						if (bestPlaceholder != null) {
-							String hover = bestPlaceholder.getHover();
-							String suggest = bestPlaceholder.getSuggest();
-							String run = bestPlaceholder.getRun();
-							message.getChatObjects().add(i + 1, new ChatObject(cc(plugin.getInternMethods().setPlaceholders(p, bestPlaceholder.getValue())), hover, suggest, run));
-							if (bestPlaceholder.isText()) {
-								message.getChatObjects().get(i + 1).setIsText(true);
-							}
-						} else {
-							message.getChatObjects().add(i + 1, new ChatObject(""));
-						}
-						message.getChatObjects().add(i + 2, new ChatObject(objMsg.substring(i3)));
+						putplaceholders(objMsg, cp, chatObj, bestPlaceholder, message, i, p);
 					}
 				}
 			}
 		}
+	}
+
+	private void putplaceholders(String objMsg, CustomPlaceholder cp, ChatObject chatObj, SubPlaceholder bestPlaceholder, ChatMessage message, int i, Player p) {
+		int i2 = objMsg.indexOf("{" + cp.getId() + "}");
+		int i3 = i2 + ("{" + cp.getId() + "}").length();
+		chatObj.setMessage(objMsg.substring(0, i2));
+		if (bestPlaceholder != null) {
+			String hover = bestPlaceholder.getHover();
+			String suggest = bestPlaceholder.getSuggest();
+			String run = bestPlaceholder.getRun();
+			message.getChatObjects().add(i + 1, new ChatObject(cc(plugin.getInternMethods().setPlaceholders(p, bestPlaceholder.getValue())), hover, suggest, run));
+			if (bestPlaceholder.isText()) {
+				message.getChatObjects().get(i + 1).setIsText(true);
+			}
+		} else {
+			message.getChatObjects().add(i + 1, new ChatObject(""));
+		}
+		message.getChatObjects().add(i + 2, new ChatObject(objMsg.substring(i3)));
 	}
 
 }
